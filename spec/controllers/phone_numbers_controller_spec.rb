8a46ee73ee0de7502b5d53180c2f7d36f5f772e4
Jason Pilz <jasonpilz@gmail.com>
2015-10-31 17:52:23 -0600

    finished implementing phone numbers

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -103,7 +103,7 @@ RSpec.describe PhoneNumbersController, type: :controller do
         expect(assigns(:phone_number)).to eq(phone_number)
       end
 
-      it "redirects to the phone_number" do
+      it "redirects to the person" do
         bob = Person.create(first_name: 'Bob', last_name: 'Jones')
         valid_attributes = { number: '555-5678', person_id: bob.id }
         phone_number = PhoneNumber.create! valid_attributes
@@ -128,6 +128,10 @@ RSpec.describe PhoneNumbersController, type: :controller do
   end
 
   describe "DELETE #destroy" do
+
+    let(:bob) { Person.create(first_name: 'Bob', last_name: 'Jones') }
+    let(:valid_attributes) { { number: '555-5678', person_id: bob.id } }
+
     it "destroys the requested phone_number" do
       phone_number = PhoneNumber.create! valid_attributes
       expect {
@@ -135,10 +139,10 @@ RSpec.describe PhoneNumbersController, type: :controller do
       }.to change(PhoneNumber, :count).by(-1)
     end
 
-    it "redirects to the phone_numbers list" do
+    it "redirects to the phone numbers' person" do
       phone_number = PhoneNumber.create! valid_attributes
       delete :destroy, {:id => phone_number.to_param}, valid_session
-      expect(response).to redirect_to(phone_numbers_url)
+      expect(response).to redirect_to(phone_number.person)
     end
   end
 
