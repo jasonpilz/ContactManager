a0f75ed38f785ae9c03edd625f100709c148b0ec
Jason Pilz <jasonpilz@gmail.com>
2015-10-31 20:32:13 -0600

    All validation tests passing

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  let(:email_address) { EmailAddress.new(address: "jasonpilz@gmail.com") }
+  let(:email_address) { EmailAddress.new(address: "jasonpilz@gmail.com",
+                                         person_id: 2) }
 
   it 'is valid' do
     expect(email_address).to be_valid
@@ -9,6 +10,11 @@ RSpec.describe EmailAddress, type: :model do
 
   it 'is invalid without an address' do
     email_address.address = nil
+    expect(email_address).not_to be_valid
+  end
+
+  it 'must have a reference to a person' do
+    email_address.person_id = nil
     expect(email_address).not_to be_valid
   end
 end
